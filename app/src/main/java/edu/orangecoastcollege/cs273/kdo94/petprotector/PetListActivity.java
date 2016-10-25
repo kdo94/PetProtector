eb9936e7f4f76998d317f595994a3e05b5a09ee8
kdo94 <kdo94@student.cccd.edu>
2016-10-25 16:30:22 -0700

    Added permissions

--- app/src/main/java/edu/orangecoastcollege/cs273/kdo94/petprotector/PetListActivity.java
+++ app/src/main/java/edu/orangecoastcollege/cs273/kdo94/petprotector/PetListActivity.java
@@ -1,14 +1,17 @@
 package edu.orangecoastcollege.cs273.kdo94.petprotector;
 
 import android.Manifest;
+import android.content.Intent;
 import android.content.pm.PackageManager;
 import android.net.Uri;
 import android.os.Bundle;
+import android.provider.MediaStore;
 import android.support.v4.app.ActivityCompat;
 import android.support.v4.content.ContextCompat;
 import android.support.v7.app.AppCompatActivity;
 import android.view.View;
 import android.widget.ImageView;
+import android.widget.Toast;
 
 import java.util.ArrayList;
 
@@ -51,12 +54,23 @@ public class PetListActivity extends AppCompatActivity {
         if (readStoragePermission != PackageManager.PERMISSION_GRANTED)
             permList.add(Manifest.permission.READ_EXTERNAL_STORAGE);
 
+        int requestCode = 100; // can make this whatever we want
         // If the list has items, we need to request permissions from the user
         if(permList.size() > 0){
             // Convert the ArrayList into an Array of strings
             String[] perms = new String[permList.size()];
 
-            ActivityCompat.requestPermissions(this, permList.toArray(perms), 0);
+            ActivityCompat.requestPermissions(this, permList.toArray(perms), requestCode);
+        }
+
+        // If we have all three permissions, open the ImageGallery
+        if(cameraPermission == PackageManager.PERMISSION_GRANTED &&
+                readStoragePermission == PackageManager.PERMISSION_GRANTED &&
+                writeStoragePermission == PackageManager.PERMISSION_GRANTED){
+            // Use an Intent to launch the gallery and take pictures
+            Intent galleryIntent = new Intent(Intent.ACTION_PICK, MediaStore.Images.Media.EXTERNAL_CONTENT_URI);
+            startActivityForResult(galleryIntent, requestCode);
+            Toast.makeText(this, "Pet Protector requires camera and external storage permission", Toast.LENGTH_SHORT);
         }
     }
 }
